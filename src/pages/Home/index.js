19613fa531586d2317581829076e6222f83117d8
Kleber Sigrist <kleberls@ciandt.com>
2021-02-08 15:39:44 -0300

    create separate functions to show different contents

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,24 +1,23 @@
 import React, { useEffect, useState } from 'react';
-import Card from "../../components/Card";
+import Card from '../../components/Card';
 
 import './style.css';
 
 function Home() {
-  const [search, setSearch] = useState('');
   const [items, setItems] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [search, setSearch] = useState('');
+  const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
 
   async function loadAllItems() {
     try {
       setLoading(true);
       const result = await fetch('https://run.mocky.io/v3/1497dad2-3f2f-4b7b-a778-34a9f5d9565c');
-      const json = await result.json();
+      const items = await result.json();
       setError(false);
-      setItems(json);
+      setItems(items);
     } catch(e) {
       setError(true);
-      setItems({});
     } finally {
       setLoading(false);
     }
@@ -34,32 +33,60 @@ function Home() {
     return items.filter(item => exp.test(item.name));
   }
 
-  useEffect(() => {
-    loadAllItems();
-  }, [])
+  function renderList() {
+    if (loading) {
+      return showLoading();
+    } else if (error) {
+      return showError();
+    } else {
+      return showCards();
+    }
+  }
+
+  function showCards() {
+    const filtered = getItems();
+
+    if (filtered.length > 0) {
+      return (
+        <div className="cards-wrapper">
+          { filtered.map((item, index) => (
+            <Card key={index} name={item.name} color={item.color} />
+          )) }
+        </div>
+      );
+    }
 
     return (
-      <div className="App">
-        <header>Buscador de Cores</header>
-        <main>
-          <div className="search-container">
-            <input type="search" className="search" placeholder="Busque pelo nome da cor" onChange={handleChangeSearch} />
-          </div>
-          { !loading
-            ? getItems().length > 0
-              ? <div className="cards-wrapper">
-                  { getItems().map((item, index) => (
-                    <Card name={item.name} color={item.color} />
-                  )) }
-                </div>
-              : !error
-                ? <p>Não há cores para essa busca.</p>
-                : <p>Ops... ocorreu um erro.</p>
-            : <p>Buscando...</p>
-          }
-        </main>
-      </div>
+      <p>Não há cores para essa busca.</p>
     );
   }
+
+  function showLoading() {
+    return (
+      <p>Buscando...</p>
+    );
+  }
+
+  function showError() {
+    return (
+      <p>Ops... ocorreu um erro.</p>
+    );
+  }
+
+  useEffect(() => {
+    loadAllItems();
+  }, []);
+
+  return (
+    <div className="App">
+      <header>Buscador de Cores</header>
+      <main>
+        <div className="search-container">
+          <input type="search" className="search" placeholder="Busque pelo nome da cor" onChange={handleChangeSearch} />
+        </div>
+        {renderList()}
+      </main>
+    </div>
+  )};
 
   export default Home;
